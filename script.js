34380280c5b2462fb21b4ae829ef6af68681858b
lukemedlin <41134833+lukemedlin@users.noreply.github.com>
2023-02-03 01:06:32 -0600

    Add submit and toggle read buttons

--- script.js
+++ script.js
@@ -7,30 +7,42 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-document
-  .querySelector('#submit-btn')
-  .addEventListener('click', addBookToLibrary);
+function removeBook() {
+  myLibrary.splice(this.dataset.id, 1);
+  showBooks();
+}
 
+function updateRead() {
+  this.textContent === 'Read'
+    ? (this.textContent = 'Not read')
+    : (this.textContent = 'Read');
+}
 function showBooks() {
-  document.querySelector('.library-grid').innerHTML = ''; 
-  
+  document.querySelector('.library-grid').innerHTML = '';
+
   for (let i = 0; i < myLibrary.length; i += 1) {
     const newDiv = document.createElement('div');
     const newTitle = document.createElement('h3');
     const newBy = document.createElement('h4');
     const newAuthor = document.createElement('h3');
     const newPages = document.createElement('h4');
-    const newRead = document.createElement('h4');
+    const newRead = document.createElement('button');
+    const newRemove = document.createElement('button');
 
     newDiv.classList.add('book-card');
+    newRemove.dataset.id = i;
+
+    newRemove.addEventListener('click', removeBook);
+    newRead.addEventListener('click', updateRead);
 
     newTitle.textContent = myLibrary[i].title;
     newBy.textContent = 'by';
     newAuthor.textContent = myLibrary[i].author;
     newPages.textContent = `${myLibrary[i].pages} pages`;
     newRead.textContent = myLibrary[i].read === 'true' ? 'Read' : 'Not read';
+    newRemove.textContent = 'Remove';
 
-    newDiv.append(newTitle, newBy, newAuthor, newPages, newRead);
+    newDiv.append(newTitle, newBy, newAuthor, newPages, newRead, newRemove);
 
     document.querySelector('.library-grid').appendChild(newDiv);
   }
@@ -45,6 +57,19 @@ function addBookToLibrary() {
   myLibrary.push(new Book(title, author, pages, read));
   showBooks();
 }
+
+function showForm() {
+  document.querySelector('.form').toggleAttribute('hidden');
+  document.querySelector('#submit-btn').toggleAttribute('hidden');
+}
+
+document
+  .querySelector('#submit-btn')
+  .addEventListener('click', addBookToLibrary);
+
+document.querySelector('#new-book-btn').addEventListener('click', showForm);
+
+
 
 const lordOfTheRings = new Book('Lord of the Rings', 'Dmitri', 345, false);
 myLibrary.push(lordOfTheRings);
